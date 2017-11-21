1a8f5a0316c512db9b3caa5cf8652866cd6b4563
Milos Gajdos <milosgajdos83@gmail.com>
2017-11-21 17:15:42 +0000

    Model selection part done including answer on validation selection

--- hmm-ai/my_model_selectors.py
+++ hmm-ai/my_model_selectors.py
@@ -68,16 +68,38 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
+    def score(self, model, n):
+        logL = model.score(self.X, self.lengths)
+        # number of free parameters are determined based on
+        # https://discussions.udacity.com/t/number-of-parameters-bic-calculation/233235/12
+        # init_p + transition_p + emission_p
+        p = n * (n-1) + (n-1) + 2 * self.X.shape[1] * n
+        return (-2 * logL) + (p * np.log(self.X.shape[0]))
+
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
 
         :return: GaussianHMM object
         """
+
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # init lowest score to the worst possible score
+        best_score = float('inf')
+        best_model = None
+
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(n)
+                model_score = self.score(model, n)
+                if model_score < best_score:
+                    best_score = model_score
+                    best_model = model
+            except:
+                pass
+
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -90,11 +112,35 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
+    def score(self, model):
+        other_score = 0.0
+        # iterate through all words and add together all their scores
+        for word in self.words:
+            if word != self.this_word:
+                X, lengths = self.hwords[word]
+                other_score += model.score(X, lengths)
+
+        return model.score(self.X, self.lengths) - (other_score/(len(self.words)-1))
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # init the best score and best model
+        best_score = float('-inf')
+        best_model = None
+
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(n)
+                model_score = self.score(model)
+                if model_score > best_score:
+                    best_score = model_score
+                    best_model = model
+            except:
+                pass
+
+        return best_model
+
 
 
 class SelectorCV(ModelSelector):
@@ -102,8 +148,32 @@ class SelectorCV(ModelSelector):
 
     '''
 
+    def score(self, n):
+        test_scores = []
+        # default n_splits is 3, we will take min of num_sequences and default
+        split_method = KFold(n_splits=min(3, len(self.sequences)))
+
+        for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+            self.X, self.lengths = combine_sequences(cv_train_idx, self.sequences)
+            model = self.base_model(n)
+            cv_test_X, cv_test_lengths = combine_sequences(cv_test_idx, self.sequences)
+            test_scores.append(model.score(cv_test_X, cv_test_lengths))
+
+        return model, np.mean(test_scores)
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        best_score = float("-inf")
+        best_model = None
+
+        for n in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model, model_score = self.score(n)
+                if model_score > best_score:
+                    best_score = model_score
+                    best_model = model
+            except:
+                pass
+
+        return best_model
